3787851e95587243b96e455ed0c891d4f138c9f1
Vivek Lodhi <lodhivivek9516@gmail.com>
2026-09-05 17:53:05 +0530

    Employee Subscription Management

---
+++ src/main/java/com/employee_subscription/EmployeeSubscriptionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.employee_subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSubscriptionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSubscriptionBackendApplication.class, args);
+	}
+
+}
